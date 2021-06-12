30486b527eb7254f076ee58ab1a6541426f3f043
marcomaret <marettimarco@outlook.it>
2021-06-12 11:24:50 +0200

    Created Book2Fest app

--- base_project/settings.py
+++ base_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'book2fest.app.Book2FestConfig'
 ]
 
 MIDDLEWARE = [
---
+++ book2fest/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Book2FestConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'book2fest'
