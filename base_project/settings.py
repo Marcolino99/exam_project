a48641e573aeeabebce1a3f8e08b13bd618e2240
marcomaret <marettimarco@outlook.it>
2021-06-12 12:30:35 +0200

    Makemigrations and migrate

--- base_project/settings.py
+++ base_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'book2fest.app.Book2FestConfig'
+    'book2fest.apps.Book2FestConfig'
 ]
 
 MIDDLEWARE = [
