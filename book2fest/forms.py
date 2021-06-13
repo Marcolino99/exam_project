6d75dc9523f758fa5a3217f123692667c8b9eed4
marcomaret <marettimarco@outlook.it>
2021-06-13 13:35:09 +0200

    Created User Profile Page
    Created a User profile page with form to apply changes

---
+++ book2fest/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from book2fest.models import UserProfile
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
+class UserProfileForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=255)
+    last_name = forms.CharField(max_length=255)
+    email = forms.EmailField()
+
+
+    helper = FormHelper()
+    helper.form_id = 'user_profile_crispy_form'
+    helper.form_method = 'POST'
+    helper.add_input(Submit('save', 'Save'))
+    helper.inputs[0].field_classes = 'btn btn-success'
+
+    class Meta:
+        model = UserProfile
+        fields = ['first_name', 'last_name', 'email', 'organizer']
+
+
+def form_validation_error(form):
+    msg = ""
+    for field in form:
+        for error in field.errors:
+            msg += "%s: %s \\n" % (field.label if hasattr(field, 'label') else 'Error', error)
+    return msg
+
