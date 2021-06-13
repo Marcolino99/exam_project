442bdb1c89598d9906440b37ec45a8633dc074fe
marcomaret <marettimarco@outlook.it>
2021-06-13 13:35:24 +0200

    Added User Profile urls

--- book2fest/urls.py
+++ book2fest/urls.py
@@ -1,4 +1,10 @@
-from django import urls
+from django.urls import path
 from . import views
+from .views import UserProfileView
 
 app_name = "book2fest"
+
+urlpatterns=[
+path('profile/', UserProfileView.as_view(), name='user-profile'),
+]
+
