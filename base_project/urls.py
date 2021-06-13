71db78dcbdd5b77900aea2aaa73081f2d54cdd18
marcomaret <marettimarco@outlook.it>
2021-06-13 13:35:24 +0200

    Added User Profile urls

--- base_project/urls.py
+++ base_project/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from base_project.views import UserCreationView, Homepage
 
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('book2fest/', include('book2fest.urls'))
 ]
