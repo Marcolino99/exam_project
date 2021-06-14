d0c560275f61c41e6a96a504c75e3d36ef6f2a0a
marcomaret <marettimarco@outlook.it>
2021-06-14 17:50:54 +0200

    Added user and organizer create paths

--- book2fest/urls.py
+++ book2fest/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from . import views
-from .views import UserProfileView, OrganizerProfileView, ChooseProfileView, ArtistCreate
+from .views import UserProfileView, OrganizerProfileView, CompleteRegistrationView, ArtistCreate, UserCreate, \
+    OrganizerCreate
 
 app_name = "book2fest"
 
 urlpatterns=[
-    path('choose-profile', ChooseProfileView.as_view(), name='choose-profile'),
+    path('complete-reg/', CompleteRegistrationView.as_view(), name='complete-reg'),
+    path('user/create', UserCreate.as_view(), name='user-create'),
     path('user/profile', UserProfileView.as_view(), name='user-profile'),
     path('organizer/profile', OrganizerProfileView.as_view(), name='organizer-profile'),
+    path('organizer/create', OrganizerCreate.as_view(), name='organizer-create'),
     path('artist/create', ArtistCreate.as_view(), name='artist-create')
 ]
-
