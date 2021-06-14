601a0dcca4328b9f8ee3b5bec717adde9cdd1de3
marcomaret <marettimarco@outlook.it>
2021-06-14 18:27:32 +0200

    Added Url and migration form Artist Create and Detail

--- book2fest/admin.py
+++ book2fest/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from book2fest.models import Artist, OrganizerProfile, UserProfile
+from book2fest.models import Artist, OrganizerProfile, UserProfile, Genre, Category
 
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(OrganizerProfile)
 admin.site.register(Artist)
+admin.site.register(Genre)
+admin.site.register(Category)
--- book2fest/urls.py
+++ book2fest/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from .views import UserProfileView, OrganizerProfileView, CompleteRegistrationView, ArtistCreate, UserCreate, \
-    OrganizerCreate
+    OrganizerCreate, ArtistDetail
 
 app_name = "book2fest"
 
@@ -11,5 +11,6 @@ urlpatterns=[
     path('user/profile', UserProfileView.as_view(), name='user-profile'),
     path('organizer/profile', OrganizerProfileView.as_view(), name='organizer-profile'),
     path('organizer/create', OrganizerCreate.as_view(), name='organizer-create'),
-    path('artist/create', ArtistCreate.as_view(), name='artist-create')
+    path('artist/create', ArtistCreate.as_view(), name='artist-create'),
+    path('artist/<int:pk>/detail', ArtistDetail.as_view(), name='artist-detail'),
 ]
